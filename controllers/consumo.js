7f829d447ac05c76053906812fc177e9e01b43ba
Andrés Vigna <andivigna@gmail.com>
2016-12-26 23:25:12 -0300

    Update consumo.js

--- controllers/consumo.js
+++ controllers/consumo.js
@@ -4,13 +4,13 @@ var config  = require('../config.js');
 var Consumo = require('../models/consumo.js');
 
 router.get('/', function(req, res){
-    res.redirect('/consumos');
+    res.redirect('/app/consumos');
 });
-router.get('/consumo', function(req, res){
+router.get('/app/consumo', function(req, res){
     res.render('consumo', { active: 'consumo', usuario: req.session.username, ip: config.server.ip, port: config.server.port });
 });
 
-router.get('/consumos', function(req, res){
+router.get('/app/consumos', function(req, res){
     Consumo.find({usuario: req.session.username}, function(err,rows1){
         if(err){
             res.status(404).send('<h4>Hubo un error:</h4> <br>' + err);
@@ -20,12 +20,15 @@ router.get('/consumos', function(req, res){
                 for(i in data1){
                     consumos.data.push(data1[i]);
                 }
-                console.log(consumos);
                 Consumo.aggregate([{$group: {_id: "$usuario", total: {$sum:"$importe"}}}],function(err, rows){
+                    var total = 0
+                    if(rows.length){
+                       total =  rows[0].total;
+                    }
                     if(err){
                         res.status(404).send('<h4>Hubo un error:</h4> <br>' + err);
                     }else{
-                        res.render('consumos', { pagina: 'inicio', usuario: req.session.username, consumos: consumos, consumoTotal: rows[0].total, consumo: rows1, ip: config.server.ip, port: config.server.port });
+                        res.render('consumos', { pagina: 'inicio', usuario: req.session.username, consumos: consumos, consumoTotal: total, consumo: rows1, ip: config.server.ip, port: config.server.port });
                     }
                 });
             });
@@ -33,7 +36,7 @@ router.get('/consumos', function(req, res){
     });
 });
 
-router.get('/estadisticas', function(req, res){
+router.get('/app/estadisticas', function(req, res){
     Consumo.find({usuario: req.session.username}, function(err,rows){
         if(err){
             res.status(404).send('<h4>Hubo un error:</h4> <br>' + err);
@@ -43,18 +46,18 @@ router.get('/estadisticas', function(req, res){
     });
 });
 
-router.post('/nuevoConsumo', function(req, res){
+router.post('/app/nuevoConsumo', function(req, res){
     var consumo = new Consumo({ usuario: req.session.username, importe: req.body.importe, descripcion: req.body.descripcion });
     consumo.save(function(err, data){
         if(err)
             res.status(404).send('<h4>Hubo un error:</h4> <br>' + err);
         else{
-            res.redirect('/consumos');
+            res.redirect('/app/consumos');
         }
     });
 });
 
-router.get('/api/getConsumos', function(req, res){
+router.get('/app/api/getConsumos', function(req, res){
     
     Consumo.find({usuario: req.session.username}, function(err,rows){
         if(err){
