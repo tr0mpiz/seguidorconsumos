6478d646f359fc590c4bbe137d7cfbad2e30576d
Andrés Vigna <andivigna@gmail.com>
2016-12-26 21:44:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,8 @@ config.server = {};
 config.db = {};
 config.db.mysql = {};
 config.db.mysql.tab = {};
-config.db.ifx = {};
 config.socket = {};
-config.imagenes = {};
-config.imagenes.planchetas = {};
+
 /*SERVER*/
 config.server.ip = process.env.IP || ip.address();
 config.server.port = process.env.PORT || 3000;
@@ -25,18 +23,5 @@ config.db.mysql.password = argv.mysqlPassword || '1234';
 config.db.mysql.db = argv.mysqlDb || 'mvl';
 config.db.mysql.tab.sessions = argv.mysqlTabSessions || 'sesiones';
 config.db.mysql.tab.users = argv.mysqlTabSessions || 'usuarios';
-/*FICHAS Y PLANCHETAS*/
-config.imagenes.fichas = 'E:\\Usuario\\Pictures\\imagfichasPNG\\'
-config.imagenes.planchetas.ano2000 = '';
-config.imagenes.planchetas.ano2005 = '';
-/*DB INFORMIX*/
-config.db.ifx.host = 'munivl_tcp';
-config.db.ifx.ip = 'onsoctcp';
-config.db.ifx.service = '1521';
-config.db.ifx.protocol = 'onsoctcp';
-config.db.ifx.db = 'catastro';
-config.db.ifx.user = 'catas01';
-config.db.ifx.password = 'catas208';
-config.db.ifx.connectionString = 'SERVER=munivl_tcp;DATABASE=catastro;HOST=192.9.200.5;PROTOCOL=onsoctcp;SERVICE=1521;UID=catas01;PWD=catas208;';
 
 module.exports = config;
